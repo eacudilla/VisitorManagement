2c227334bfd53bbb739981b55422b68e57e8e06c
eacudilla <64538192+eacudilla@users.noreply.github.com>
2020-06-26 20:19:52 +0800

    add new compReg.js

--- addCompany.php
+++ addCompany.php
@@ -24,7 +24,7 @@ include('session.php');
   <link rel="stylesheet" href="./style.css" />
   <link rel="stylesheet" href="operation.css">
   <script src="lodash.js"></script>
-  <script src="/js/addNew.js"></script>
+  <script src="/js/compReg.js"></script>
 
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -72,7 +72,7 @@ include('session.php');
                   
                   
                   <div class="btn-group">
-                    <input type = "button" class="btn btn-info btn-sm" id="btn-submit" name="btn-submit" value="Submit" onclick = "submitCompReg()">Submit</button>
+                    <button type = "button" class="btn btn-info btn-sm" id="btn-submit" name="btn-submit" value="Submit" onclick = "submitCompReg()">Submit</button>
                     <button type = "button" class="btn btn-info btn-sm" name="reset" value="Reset">Reset</button>
 
                    
