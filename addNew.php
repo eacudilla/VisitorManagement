87223e4a28e36c57838127f37612d70a87bf84fb
eacudilla <64538192+eacudilla@users.noreply.github.com>
2020-06-27 18:02:26 +0800

    Update addNew.php

--- addNew.php
+++ addNew.php
@@ -33,36 +33,6 @@ include('session.php');
 </head>
 
 
-<!-- Modal -->
-<div class="modal fade" data-backdrop="false" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>dfsdffffffffffffffffffffffffffffffffffff
-          asfasfasfsaf
-          asfasfasf
-          asfasfasf
-          array_shiftasf
-          asf
-          array_shiftasf
-          asf
-          asf
-        </p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" id="closeModal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
 <body onload="startStreaming()">
   <main>
     <div class="container text-center" >
@@ -194,6 +164,36 @@ include('session.php');
 
  
 
+
+<!-- Modal -->
+<div class="modal fade" data-backdrop="false" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>dfsdffffffffffffffffffffffffffffffffffff
+          asfasfasfsaf
+          asfasfasf
+          asfasfasf
+          array_shiftasf
+          asf
+          array_shiftasf
+          asf
+          asf
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" id="closeModal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
