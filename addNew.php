84bf556c5dd44b9d9e33643e5e0e039555a56a2c
eacudilla <64538192+eacudilla@users.noreply.github.com>
2020-06-27 11:38:32 +0800

    Update addNew.php

--- addNew.php
+++ addNew.php
@@ -87,7 +87,9 @@ include('session.php');
                     <button type="button" class="btn btn-info btn-sm" id="btn-capture" name="btn-capture">Capture</button>
                     <button type="button" class="btn btn-info btn-sm" name="reset" value="Reset">Reset</button>
                     <button type="button" class="btn btn-info btn-sm" id="btn-start" >Re-shot</button>
-                   
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                     Launch demo modal
+                        </button>
               
                   </div>
                  </form>
@@ -155,7 +157,26 @@ include('session.php');
     
   </main>
 
-
+<!-- Modal -->
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
   <script>
    
