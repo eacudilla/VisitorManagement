c2db8c67c6222801cd05b50a63a9f2e93ac18c41
eacudilla <64538192+eacudilla@users.noreply.github.com>
2020-06-27 17:43:39 +0800

    Update addNew.php

--- addNew.php
+++ addNew.php
@@ -16,6 +16,8 @@ include('session.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>Visitor Monitoring Software</title>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
   integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
@@ -26,7 +28,7 @@ include('session.php');
   <script src="lodash.js"></script>
   <script src="/js/addNew.js"></script>
   
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  
   
 </head>
 
