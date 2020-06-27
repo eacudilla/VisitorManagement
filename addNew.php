b482c7dca504691a84b44633dc8cb9b1bccad3b0
eacudilla <64538192+eacudilla@users.noreply.github.com>
2020-06-27 16:34:50 +0800

    Update addNew.php

--- addNew.php
+++ addNew.php
@@ -27,7 +27,8 @@ include('session.php');
   <script src="/js/addNew.js"></script>
   
 
-
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" 
+  integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
