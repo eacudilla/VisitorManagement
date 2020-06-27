82031f592e6fc9cc0dc265a763d824fc2a2ab95b
eacudilla <64538192+eacudilla@users.noreply.github.com>
2020-06-27 16:37:10 +0800

    Update addNew.php

--- addNew.php
+++ addNew.php
@@ -26,10 +26,10 @@ include('session.php');
   <script src="lodash.js"></script>
   <script src="/js/addNew.js"></script>
   
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" 
   integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ 
 </head>
 
 <body onload="startStreaming()">
